31833efe2cb748e6ac3ade0ddc32852c7798d2b5
neerajs-28 <neerajs28288@gmail.com>
2018-06-30 00:15:50 +0530

    adding files

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist/event-app'));
+app.use(express.static(__dirname+'/dist'));
 app.listen(process.env.PORT||5000);
 
 app.get("/*",function(req,res){
 
-    res.sendFile(path.join(__dirname + '/dist/event-app/index.html'));
+    res.sendFile(path.join(__dirname + '/distindex.html'));
 })
